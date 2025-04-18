5698f2bf48807cdd2b621e10911b956a55f4117b
mononoken <81536479+mononoken@users.noreply.github.com>
2025-04-17 21:33:20 -0700

    Complete NAVI and WNAV

--- config/corne.h
+++ config/corne.h
@@ -1,6 +1,12 @@
 #define BASE 0
 #define NUMB 1
-#define NAVI 2
-#define SYMB 3
-#define LSYM 4
-#define RSYM 5
+#define SYMB 2
+#define LSYM 3
+#define RSYM 4
+#define NAVI 5
+#define WNAV 6
+
+#define TMUX_LT &kp LC(LA(LEFT))
+#define TMUX_DN &kp LC(LA(DOWN))
+#define TMUX_UP &kp LC(LA(UP))
+#define TMUX_RT &kp LC(LA(RIGHT))
