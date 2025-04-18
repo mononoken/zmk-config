149211e4055f71658415dbbcdc78122800b0e94c
mononoken <81536479+mononoken@users.noreply.github.com>
2025-04-17 21:51:32 -0700

    Revert wnav

--- config/corne.h
+++ config/corne.h
@@ -5,8 +5,3 @@
 #define RSYM 4
 #define NAVI 5
 #define WNAV 6
-
-#define TMUX_LT &kp LC(LA(LEFT))
-#define TMUX_DN &kp LC(LA(DOWN))
-#define TMUX_UP &kp LC(LA(UP))
-#define TMUX_RT &kp LC(LA(RIGHT))
