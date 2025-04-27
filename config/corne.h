33daaafa19600b35a20601f4acc436c45abb9ea2
mononoken <81536479+mononoken@users.noreply.github.com>
2025-04-26 22:19:48 -0700

    Add nght layer toggle option

--- config/corne.h
+++ config/corne.h
@@ -1,11 +1,12 @@
 #define BASE 0
-#define NUMB 1
-#define SYMB 2
-#define LSYM 3
-#define RSYM 4
-#define NAVI 5
-#define WNAV 6
-#define SHRT 7
+#define NGHT 1
+#define NUMB 2
+#define SYMB 3
+#define LSYM 4
+#define RSYM 5
+#define NAVI 6
+#define WNAV 7
+#define SHRT 8
 
 #define TMUX_LT &kp LC(LA(LEFT))
 #define TMUX_DN &kp LC(LA(DOWN))
