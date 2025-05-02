b601808301647ec10ae80602ea5cd4c0bd8a86f6
mononoken <81536479+mononoken@users.noreply.github.com>
2025-05-02 15:28:10 -0700

    Squash merge night-reverse into main

--- config/corne.h
+++ config/corne.h
@@ -1,11 +1,9 @@
 #define BASE 0
 #define NUMB 1
 #define SYMB 2
-#define LSYM 3
-#define RSYM 4
-#define NAVI 5
-#define WNAV 6
-#define SHRT 7
+#define NAVI 3
+#define WNAV 4
+#define SHRT 5
 
 #define TMUX_LT &kp LC(LA(LEFT))
 #define TMUX_DN &kp LC(LA(DOWN))
